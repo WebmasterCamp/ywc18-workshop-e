955e9fb9a1f61f9375d78f04d2420198b221640c
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-08-14 14:47:18 +0700

    update index add zoom google img

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,5 @@
 <template lang="pug">
-  //- .container.max-w-container.mx-auto.w-full
-  //-   .container.max-w-container.mx-auto.w-full
-  //-     .text-5xl Key Tech
-  //-     .text-3xl คือกุญแจที่จะเชื่อมคุณให้เข้ากับโลกเทคโนโลยีได้ง่ายมากยิ่งขึ้น
-  //-   .max-w-container.mx-auto.w-full
-  //-     #interrest-lesson.max-w-container.mx-auto.w-full
-  //-       .text-3xl บทเรียนที่น่าสนใจ
-  //-       Content
-  //-     #popular
-  //-       .text-3xl หมวดหมู่ยอดนิยม
   .container.max-w-container.mx-auto.w-full.pb-12.px-4
-    //- CTAContact
     #search.mb-8
       SearchBar
     .grid.grid-cols-3
@@ -22,19 +11,22 @@
         div
           Content
     hr.text-3xl.m-2.my-8
-    .grid.grid-cols-3.m-2
-      .text-3xl Google
+    .grid.grid-cols-3.m-2.bg-gray-200
+      .flex.items-center.justify-center
+        img(src="@/assets/google.png")
+        .text-3xl.text-center Google
       div
         Content
       div
         Content
     hr.text-3xl.m-2.my-8
-    .grid.grid-cols-3.m-2
-      .text-3xl Zoom
+    .grid.grid-cols-3.m-2.bg-gray-200
+      .flex.items-center.justify-center
+        img(src="@/assets/zoom.png")
+        .text-3xl.text-center Zoom
       div
         Content
       div
-        img.mb-4(src="~/assets/test.png")
         Content
 </template>
 
