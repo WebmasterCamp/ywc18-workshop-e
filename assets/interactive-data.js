3f92a045860b3865fcfbe9d43b152f139e2c2236
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2021-08-14 17:16:21 +0900

    Add interactive app

--- assets/interactive-data.js
+++ assets/interactive-data.js
@@ -1,17 +1,80 @@
 export default {
   test: [
     {
-      image: '/ia/test-01.png',
+      image: '/ia-zoom/01.jpg',
       description: '',
       points: [
-        { x: 45.51, y: 78.31, size: 24 }
+        { x: 45.84, y: 31.04, size: 64 }
       ]
     },
     {
-      image: '/ia/test-02.png',
+      image: '/ia-zoom/02.jpg',
       description: '',
       points: [
-        { x: 78.28, y: 59.41, size: 36 }
+        { x: 50.24, y: 30.71, size: 156 }
+      ]
+    },
+    {
+      image: '/ia-zoom/03.jpg',
+      description: '',
+      points: [
+        { x: 49.9, y: 46.07, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/04.jpg',
+      description: '',
+      points: [
+        { x: 4.08, y: 96.05, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/05.jpg',
+      description: '',
+      points: [
+        { x: 4.08, y: 96.05, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/06.jpg',
+      description: '',
+      points: [
+        { x: 18.36, y: 95.81, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/07.jpg',
+      description: '',
+      points: [
+        { x: 18.36, y: 95.81, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/08.jpg',
+      description: '',
+      points: [
+        { x: 54.22, y: 95.47, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/09.jpg',
+      description: '',
+      points: [
+        { x: 98.65, y: 24.14, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/10.jpg',
+      description: '',
+      points: [
+        { x: 94.83, y: 96.45, size: 64 }
+      ]
+    },
+    {
+      image: '/ia-zoom/11.jpg',
+      description: '',
+      points: [
+        { x: 84.59, y: 87.94, size: 64 }
       ]
     }
   ]
