405a85f21eebb1edf0adebc08143cf0bd3b879f9
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2021-08-14 20:41:55 +0900

    Update interactive back btn

--- pages/interactive.vue
+++ pages/interactive.vue
@@ -1,8 +1,8 @@
 <template lang="pug">
 .w-full.max-w-container.mx-auto
-  nuxt-link.text-xl.mx-2.mt-8.flex.items-center.justify-start.text-gray-900.px-4.py-4.leading-none.text-center(to="/content/zoom"
-        class="hover:shadow-lg hover:bg-white-400 hover:underline" style="color: #0A0A75"
-        ) < กลับไปหน้าบทความ
+  nuxt-link.text-xl.mx-2.mt-8.inline-flex.items-center.justify-start.text-gray-900.px-4.py-4.leading-none.text-center.mb-8(to="/content/zoom"
+      class="hover:bg-white-400 hover:underline" style="color: #0A0A75"
+      ) < กลับไปหน้าบทความ
   InteractiveApp(:dataset="interactiveDataset" @done="onDone")
 </template>
 
