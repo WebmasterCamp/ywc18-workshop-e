8c83ea52139ffa0254494c8b3f163ce18ae8558a
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-08-14 16:00:26 +0700

    update zoom logo and content

--- components/Content.vue
+++ components/Content.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 nuxt-link.block.p-4.content(:to="to" class="hover:shadow-lg")
-  img.mb-4(src="~/assets/test.png")
+  img.mb-4(src="~/assets/zoom-logo.png")
   .text-3xl {{ title }}
   .text-xl {{ describe }}
 </template>
@@ -11,6 +11,7 @@ import Vue from 'vue'
 export default Vue.extend({
   props: {
     to: { type: String, default: '/' },
+    imgsrc: { type: String, default: '~/assets/zoom-logo.png' },
     title: { type: String, default: 'lorem ipsum' },
     describe: { type: String, default: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }
   }
