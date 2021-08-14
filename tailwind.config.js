62ec9ec6b8ffb88ff1a45661214b7d7adf7f132d
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2021-08-14 13:30:16 +0900

    Add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      maxWidth: {
+        container: '1232px',
+      },
+      // fontFamily: {
+      //   sans: 'Sarabun, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"',
+      // },
+    },
+  },
+};
