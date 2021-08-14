a799b3a44c1010b38db3fa2f6bb27ada1b14a39c
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2021-08-14 16:02:41 +0900

    Modify logo header

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,14 @@
 .default-layout.font-sans
   nav.fixed.top-0.right-0.left-0.z-20.hidden(class="sm:block")
     .bg-primary-700
-      .h-20.max-w-container.px-4.mx-auto.flex.items-center.text-white
+      .h-24.max-w-container.px-4.mx-auto.flex.items-center.text-white
         .flex.items-center.h-full.w-full.text.text-center.leading-tight
           .flex.items-center.flex-wrap.-mx-2
             .h-10.flex.items-center.px-2.relative.cursor-default
               nuxt-link.block.text-lg(class="hover:opacity-75" to="/" )
                 .flex
-                  .text-3xl Key Tech
-                  logo
+                  //- .text-3xl Key Tech
+                  .w-12: Logo
           .flex-1
           li.h-10.flex.items-center
             nuxt-link.block.text-lg.mx-4(class="hover:opacity-75" to="/lesson/work" )
