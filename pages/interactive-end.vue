91c62fd50dac62ed623fd1d57ee3a10fcc08abe1
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-08-14 17:38:44 +0700

    add end

--- pages/interactive-end.vue
+++ pages/interactive-end.vue
@@ -1,8 +1,14 @@
 <template lang="pug">
 .interactive-end
   .py-16
-    img(style="width: 560px;" src="interactive-end.png")
-
+    .flex.items-center.mx-auto.justify-center
+      img(style="width: 560px;" src="@/assets/interactive-end.png")
+    nuxt-link.text-2xl.mx-2.mt-8.flex.items-center.justify-center.bg-primary-700.px-4.py-4.leading-none.text-center(to="/content/zoom"
+                    class="hover:shadow-lg hover:bg-white-400" style="color: white"
+                    ) กลับไปอ่านบทความ
+    nuxt-link.text-2xl.mx-2.mt-8.flex.items-center.justify-center.text-gray-900.px-4.py-4.leading-none.text-center(to="/"
+                    class="hover:shadow-lg hover:bg-white-400" style="color: #0A0A75"
+                    ) กลับไปหน้าแรก
 </template>
 
 <script>
