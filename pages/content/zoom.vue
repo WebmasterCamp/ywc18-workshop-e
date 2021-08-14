21df1d558016fd8c0c53a615e876c7390b22f864
Chaiyapat Tantiworachot <chaiyapat@pix7.me>
2021-08-14 18:32:01 +0900

    Add sound in content

--- pages/content/zoom.vue
+++ pages/content/zoom.vue
@@ -12,41 +12,45 @@ div
             //-     p ส่งต่อให้เพื่อนของคุณ
         hr.m-2
         #WHATIS.mt-5
-            .container.max-w-container.mx-auto.w-full.pb-12.p-5.bg-gray-200
-                .text-4xl Zoom คืออะไร ?
-                .text-2xl โปรแกรมซูม (Zoom Meeting) เป็นเครื่องมือพูดคุยผ่านวิดีโอ ส่งภาพและเสียง ให้แก่ผู้ร่วมประชุม ประกอบด้วย ฟังก์ชันที่ใช้งานง่าย เลือกเปลี่ยนภาพพื้นหลัง เพื่อพูดคุยให้เข้ากับโอกาสได้ตามความเหมาะสม รวมถึงใช้เรียนออนไลน์ได้ไม่สะดุด วิธีการเข้าใช้งาน Zoom Meeting ทำได้ง่าย ทั้งจากในมือถือ และคอมพิวเตอร์
+            .container.max-w-container.mx-auto.w-full.p-8.bg-gray-200
+                .text-3xl Zoom คืออะไร ?
+                .text-xl โปรแกรมซูม (Zoom Meeting) เป็นเครื่องมือพูดคุยผ่านวิดีโอ ส่งภาพและเสียง ให้แก่ผู้ร่วมประชุม ประกอบด้วย ฟังก์ชันที่ใช้งานง่าย เลือกเปลี่ยนภาพพื้นหลัง เพื่อพูดคุยให้เข้ากับโอกาสได้ตามความเหมาะสม รวมถึงใช้เรียนออนไลน์ได้ไม่สะดุด วิธีการเข้าใช้งาน Zoom Meeting ทำได้ง่าย ทั้งจากในมือถือ และคอมพิวเตอร์
             .flex.items-center.justify-center.mt-8
-                nuxt-link.h-10.px-6.flex.items-center.text-gray-900.bg-orange.text-2xl.rounded.leading-none(to="/interactive"
+                nuxt-link.py-4.px-6.flex.items-center.text-gray-900.bg-orange.text-xl.rounded.leading-none.font-semibold(
+                  to="/interactive"
                   class="hover:shadow-lg hover:bg-white-400"
                   ) ลองเล่น Zoom ทีละขั้นตอน
-        //- audio(controls='' preload='none' style='width:480px;')
-        //-     source(src='@/static/zoomSound.m4a' type='audio/mp4')
-        //-     p Your browser does not support HTML5 audio.
+
+        .flex.items-center.justify-center.my-12.sticky.bg-gray-100.py-6(style="top: 5rem;")
+          .text-lg.mr-12 ฟังวิธีใช้งาน
+          div(style="width: 480px;")
+            audio(controls='' preload='none' style='width:480px;')
+              source(src='/zoomSound.mp3' type='audio/mp4')
 
         #CONTENT.mt-5
-            .text-4xl ใช้ Zoom บน Desktop
-            .text-2xl 1. คลิ๊กเข้าปุ่มสีฟ้า ช่องสองที่มีเครื่องหมาย + ตรงกลาง
-            .px-16: img.mb-4(src="~/assets/zoom-gif/1.gif").p-5
-            .text-2xl 2. กดที่บรรทัดแรก พิมพ์ใส่รหัสหรือลิงค์ที่ได้รับมา
-            .px-16: img.mb-4(src="~/assets/zoom-gif/2.gif").p-5
-            .text-2xl 3. บรรทัดต่อไป พิมพ์ชื่อตัวเอง (เป็นชื่อที่จะใช้แสดงตัวตนของเราในการประชุม)
-            .px-16: img.mb-4(src="~/assets/zoom-gif/3.gif").p-5
-            .text-2xl 4. กดเข้า join บรรทัดถัดมา เพื่อเข้าการประชุม
-            .px-16: img.mb-4(src="~/assets/zoom-gif/4.gif").p-5
-            .text-2xl 5. ปุ่มแรก เริ่มจากด้านซ้ายล่าง คือปุ่มเปิดปิดไมค์ <b> เส้นสีแดงคั่นกลางไมค์ = ไมค์ปิดอยู่ ไมค์สีเทา = ไมค์เปิดอยู่ </b>
-            .px-16: img.mb-4(src="~/assets/zoom-gif/5.gif").p-5
-            .text-2xl 6. ปุ่มที่ 2 คือ การเปิดปิด กล้อง จะคล้ายๆปุ่มแรกไมค์ตรง
-            .px-16: img.mb-4(src="~/assets/zoom-gif/6.gif").p-5
+            .text-3xl.mb-6 ใช้ Zoom บน Desktop
+            .text-2xl 1. เปิด Google Chrome และเข้าลิงก์ Zoom.us
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/1.gif").p-5
+            .text-2xl 2. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/2.gif").p-5
+            .text-2xl 3. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/3.gif").p-5
+            .text-2xl 4. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/4.gif").p-5
+            .text-2xl 5. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/5.gif").p-5
+            .text-2xl 6. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/6.gif").p-5
             .text-2xl 7. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
-            .px-16: img.mb-4(src="~/assets/zoom-gif/7.gif").p-5
-            .text-2xl 8.1. หากเราต้องการกดยกเลิกการประชุมให้กดปุ่ม  สีแดง การประชุมจะถูกยกเลิกทันที
-            .px-16: img.mb-4(src="~/assets/zoom-gif/8.gif").p-5
-            .text-2xl 8.2. หากกกดปุ่มสีเทา = หมายความว่าเราจะออกจากการประชุมคนเดียว แต่การประชุมจะดำเนินต่อไป
-            .px-16: img.mb-4(src="~/assets/zoom-gif/9.gif").p-5
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/7.gif").p-5
+            .text-2xl 8. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/8.gif").p-5
+            .text-2xl 9. คลิกที่ “Sign In” เพื่อสมัครสมาชิกก่อนเข้าใช้งาน หรือกด “Join” เพื่อเข้าใช้งานครั้งเดียว โดยไม่ต้องสมัครสมาชิก
+            .flex.justify-center: img.mb-4(style="max-width: 625px;" src="~/assets/zoom-gif/9.gif").p-5
             .text-2xl.text-center.text-gray-900 ติดปัญหาการใช้งาน
             .flex.items-center.justify-center.mt-8
-                nuxt-link.h-10.px-6.flex.items-center.text-gray-900.bg-gray-extend.text-2xl.rounded.leading-none(to="#"
-                    class="hover:shadow-lg hover:bg-white-400"
+                nuxt-link.py-4.px-8.flex.items-center.text-gray-900.bg-gray-300.text-lg.rounded.leading-none(to="#"
+                    class="hover:shadow-lg hover:bg-gray-200"
                     ) โทรหาฝ่ายบริการ
     CTAContact
 </template>
