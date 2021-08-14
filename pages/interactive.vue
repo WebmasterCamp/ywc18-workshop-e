2cd4c7ee48e0f527867e8d99fab9d567fc3b5543
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-08-14 18:38:54 +0700

    add back to zoom

--- pages/interactive.vue
+++ pages/interactive.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
 .w-full.max-w-container.mx-auto
+  nuxt-link.text-xl.mx-2.mt-8.flex.items-center.justify-start.text-gray-900.px-4.py-4.leading-none.text-center(to="/content/zoom"
+        class="hover:shadow-lg hover:bg-white-400 hover:underline" style="color: #0A0A75"
+        ) < กลับไปหน้าบทความ
   InteractiveApp(:dataset="interactiveDataset" @done="onDone")
 </template>
 
