0ff6b856f763beabbee344d4bd1146ba6425cce2
Siriwat <siriwatjaiyungyuen@gmail.com>
2021-08-14 14:34:13 +0700

    add zoom page

--- components/Content.vue
+++ components/Content.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
     .content
         img.mb-4(src="~/assets/test.png")
-        nuxt-link.text-3xl(class="hover:underline" to="/" ) ใช้ Zoom ยังไง
+        nuxt-link.text-3xl(class="hover:underline" to="/content/zoom" ) ใช้ Zoom ยังไง
         .text-xl Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
 </template>
